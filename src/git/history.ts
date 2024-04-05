5a3416a9099aeb2f4d2add55d593f3c5762fed30
Templum <templum.dev@gmail.com>
2024-04-05 21:46:44 +0000

    :recycle: Using exec over native module

--- src/git/history.ts
+++ src/git/history.ts
@@ -1,6 +1,6 @@
 import { debug, warning } from '@actions/core';
-import { spawn } from 'node:child_process';
-import { cwd } from 'node:process';
+import { getExecOutput } from '@actions/exec';
+import { isDebugging } from '../shared/environment.js';
 
 export type GitCommit = {
     hash: {
@@ -17,135 +17,98 @@ export type GitCommit = {
 export async function getWholeHistory(workDir: string): Promise<GitCommit[]> {
     const history: GitCommit[] = [];
 
-    const git = spawn('git', ['log', '--pretty=format:"%H|||%h|||%s|||%an|||%ae"'], {
-        cwd: workDir === '' ? cwd() : workDir,
-        shell: true,
-        timeout: 20 * 1000,
+    const { exitCode, stderr, stdout } = await getExecOutput('git', ['log', '--pretty=format:"%H|||%h|||%s|||%an|||%ae"'], {
+        cwd: workDir,
+        silent: !isDebugging(),
     });
 
-    git.stdout.on('data', (current: Buffer) => {
-        const rawString = current.toString();
+    if (exitCode === 0) {
+        const outputLines = stdout.split('\n');
 
-        if (rawString.includes('|||')) {
-            const [long, short, message, name, email] = rawString.split('|||');
+        for (const output of outputLines) {
+            if (output.includes('|||')) {
+                const [long, short, message, name, email] = output.split('|||');
 
-            const historyEntry: GitCommit = {
-                hash: {
-                    long: long.startsWith('\n') ? long.replace('\n', '') : long,
-                    short: short.startsWith('\n') ? short.replace('\n', '') : short,
-                },
-                message,
-                author: {
-                    name,
-                    email,
-                },
-            };
+                const historyEntry: GitCommit = {
+                    hash: {
+                        long: long.startsWith('\n') ? long.replace('\n', '') : long,
+                        short: short.startsWith('\n') ? short.replace('\n', '') : short,
+                    },
+                    message,
+                    author: {
+                        name,
+                        email,
+                    },
+                };
 
-            history.push(historyEntry);
-            debug(`Adding ${historyEntry.hash.short} to history`);
-        } else {
-            warning(`Received unexpected data ${rawString}`);
-        }
-    });
-
-    return new Promise<GitCommit[]>((resolve, reject) => {
-        let caughtError: string | undefined = undefined;
-
-        git.stderr.on('data', (current: Buffer) => {
-            caughtError = current.toString();
-        });
-
-        git.on('close', (code) => {
-            if (code === null || code === 0) {
-                return resolve(history);
+                history.push(historyEntry);
+                debug(`Adding ${historyEntry.hash.short} to history`);
+            } else {
+                warning(`Received unexpected data ${output}`);
             }
-            return reject(new Error(caughtError === undefined ? `Getting git history failed with ${code}` : caughtError));
-        });
-    });
+        }
+
+        return history;
+    }
+
+    throw new Error(stderr);
 }
 
 export async function getHistoryFrom(workDir: string, lastTag: string): Promise<GitCommit[]> {
     const history: GitCommit[] = [];
 
-    const git = spawn('git', ['log', `${lastTag}..HEAD`, '--pretty=format:"%H|||%h|||%s|||%an|||%ae"'], {
-        cwd: workDir === '' ? cwd() : workDir,
-        shell: true,
-        timeout: 20 * 1000,
+    const { exitCode, stderr, stdout } = await getExecOutput('git', ['log', `${lastTag}..HEAD`, '--pretty=format:"%H|||%h|||%s|||%an|||%ae"'], {
+        cwd: workDir,
+        silent: !isDebugging(),
     });
 
-    git.stdout.on('data', (current: Buffer) => {
-        const rawString = current.toString();
+    if (exitCode === 0) {
+        const outputLines = stdout.split('\n');
 
-        if (rawString.includes('|||')) {
-            const [long, short, message, name, email] = rawString.split('|||');
+        for (const output of outputLines) {
+            if (output.includes('|||')) {
+                const [long, short, message, name, email] = output.split('|||');
 
-            const historyEntry: GitCommit = {
-                hash: {
-                    long: long.startsWith('\n') ? long.replace('\n', '') : long,
-                    short: short.startsWith('\n') ? short.replace('\n', '') : short,
-                },
-                message,
-                author: {
-                    name,
-                    email,
-                },
-            };
+                const historyEntry: GitCommit = {
+                    hash: {
+                        long: long.startsWith('\n') ? long.replace('\n', '') : long,
+                        short: short.startsWith('\n') ? short.replace('\n', '') : short,
+                    },
+                    message,
+                    author: {
+                        name,
+                        email,
+                    },
+                };
 
-            history.push(historyEntry);
-            debug(`Adding ${historyEntry.hash.short} to history`);
-        } else {
-            warning(`Received unexpected data ${rawString}`);
-        }
-    });
-
-    return new Promise<GitCommit[]>((resolve, reject) => {
-        let caughtError: string | undefined = undefined;
-
-        git.stderr.on('data', (current: Buffer) => {
-            caughtError = current.toString();
-        });
-
-        git.on('close', (code) => {
-            if (code === null || code === 0) {
-                return resolve(history);
+                history.push(historyEntry);
+                debug(`Adding ${historyEntry.hash.short} to history`);
+            } else {
+                warning(`Received unexpected data ${output}`);
             }
-            return reject(new Error(caughtError === undefined ? `Getting git history failed with ${code}` : caughtError));
-        });
-    });
+        }
+
+        return history;
+    }
+
+    throw new Error(stderr);
 }
 
 export async function getTag(workDir: string, version: string): Promise<string | undefined> {
-    let foundTag: string | undefined = undefined;
-
-    const git = spawn('git', ['tag', '-l'], {
-        cwd: workDir === '' ? cwd() : workDir,
-        shell: true,
-        timeout: 20 * 1000,
+    const { exitCode, stderr, stdout } = await getExecOutput('git', ['tag', '-l'], {
+        cwd: workDir,
+        silent: !isDebugging(),
     });
 
-    git.stdout.on('data', (current: Buffer) => {
-        const tags = current.toString().split('\n');
-
-        for (const tag of tags) {
-            if (tag.includes(version)) {
-                foundTag = tag;
-                break;
-            }
+    if (exitCode === 0) {
+        const potentialTag = stdout.split('\n').filter((current) => current.includes(version));
+        if (potentialTag.length > 1) {
+            warning(`Multiple Tags found that include ${version}: ${potentialTag.join(',')}, will choose ${potentialTag[0]}`);
+            return potentialTag[0];
         }
-    });
 
-    return new Promise<string | undefined>((resolve, reject) => {
-        let caughtError: string | undefined = undefined;
+        return potentialTag.length === 1 ? potentialTag[0] : undefined;
+    }
 
-        git.stderr.on('data', (current: Buffer) => {
-            caughtError = current.toString();
-        });
-
-        git.on('close', (code) => {
-            if (code === null || code === 0) {
-                return resolve(foundTag);
-            }
-            return reject(new Error(caughtError === undefined ? `Getting tag for version ${version} failed with ${code}` : caughtError));
-        });
-    });
+    throw new Error(stderr);
 }
