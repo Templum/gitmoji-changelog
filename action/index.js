468ea398e843048d09010f726dfea291a2a732c6
Templum <templum.dev@gmail.com>
2024-04-05 16:12:24 +0000

    :package: Updated compiled action

--- action/index.js
+++ action/index.js
@@ -26557,8 +26557,12 @@ async function getTag(workDir, version) {
         timeout: 20 * 1000,
     });
     git.stdout.on('data', (current) => {
-        if (current.toString().includes(version)) {
-            foundTag = current.toString();
+        const tags = current.toString().split('\n');
+        for (const tag of tags) {
+            if (tag.includes(version)) {
+                foundTag = tag;
+                break;
+            }
         }
     });
     return new Promise((resolve, reject) => {
@@ -27504,7 +27508,7 @@ async function main() {
             (0,core.warning)(`No Tag available for ${version} either create a tag or delete the existing CHANGELOG to have a new one generated`);
             throw new Error(`Was not able to discover a tag that is linked to ${version}, hence can't extend CHANGELOG`);
         }
-        const history = await getHistoryFrom(path, version);
+        const history = await getHistoryFrom(path, relatedTag);
         const addition = generateChangelog(history, currentVersion);
         await writeChangelog(path, addition, false);
     }
