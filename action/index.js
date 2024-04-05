899aaa16653d41ae2b5d5a4ce3a15a9e9bf9e39c
Templum <templum.dev@gmail.com>
2024-04-05 16:42:14 +0000

    :package: Updated compiled action

--- action/index.js
+++ action/index.js
@@ -27508,6 +27508,7 @@ async function main() {
             (0,core.warning)(`No Tag available for ${version} either create a tag or delete the existing CHANGELOG to have a new one generated`);
             throw new Error(`Was not able to discover a tag that is linked to ${version}, hence can't extend CHANGELOG`);
         }
+        (0,core.info)(`Based on version ${version} following Git TAG was identified ${relatedTag}`);
         const history = await getHistoryFrom(path, relatedTag);
         if (history.length === 0) {
             (0,core.info)(`Found no changes in history from ${relatedTag} -> HEAD`);
