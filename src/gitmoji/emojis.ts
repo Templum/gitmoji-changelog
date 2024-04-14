07e35d37b68ef642961ce1df8fef9fb96d439718
Templum <templum.dev@gmail.com>
2024-04-14 10:35:43 +0000

    :fire: Removed dead code and uneeded code

--- src/gitmoji/emojis.ts
+++ src/gitmoji/emojis.ts
@@ -12,26 +12,10 @@ export function getEmojiName(emojiOrCode: string): string {
 
 export function getEmoji(emojiName: string): string {
     for (const current of gitmojis) {
-        if (emojiName.includes('_') && current.name === emojiName.replaceAll('_', '-')) {
-            return current.emoji;
-        }
-
         if (current.name === emojiName) {
             return current.emoji;
         }
     }
 
     return '';
-}
-
-export function findEmoji(message: string): string {
-    const firstIdx = message.indexOf(':', 0);
-    const secondIdx = message.indexOf(':', firstIdx + 1);
-
-    if (firstIdx === -1 || secondIdx === -1) {
-        // Look at first char
-        return getEmojiName(message.substring(0, 1));
-    }
-
-    return message.substring(firstIdx + 1, secondIdx);
 }
